4a765a52712c723922b67a42f4e6fe654eec2ec9
Gil Alves <gildson_af@outlook.com>
2023-04-01 19:53:26 -0300

    test: Ensure RemoteAuthentication throws UnexpectedError on 404

--- test/data/use_cases/remote_authentication_test.dart
+++ test/data/use_cases/remote_authentication_test.dart
@@ -2,9 +2,11 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+// Domain
 import 'package:flutter_clean_architecture/domain/use_cases/use_cases.dart';
 import 'package:flutter_clean_architecture/domain/exceptions/exceptions.dart';
 
+// Data
 import 'package:flutter_clean_architecture/data/http/http.dart';
 import 'package:flutter_clean_architecture/data/use_cases/use_cases.dart';
 
@@ -50,6 +52,21 @@ void main() {
       method: anyNamed('method'),
       body: anyNamed('body'),
     )).thenThrow(HttpError.badRequest);
+
+    // Act
+    final future = sut.auth(params);
+
+    // Assert
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('should throw UnexpectedError if HttpClient return 404', () async {
+    // Arrange
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenThrow(HttpError.notFound);
 
     // Act
     final future = sut.auth(params);
